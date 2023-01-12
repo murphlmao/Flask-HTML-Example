20f19a1db7be9731b718385e6f4e3a2ee1cdbd05
murphlmao <murphyjmalcolm@gmail.com>
2023-01-11 20:04:41 -0500

    initial commit

---
+++ OOP_inheritance.py
@@ -0,0 +1,26 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        
+    def speak(self):
+        print("Some animal sound")
+
+class Dog(Animal):
+    def speak(self):
+        super().speak()
+        print("Woof!")
+        
+class Cat(Animal):
+    def speak(self):
+        super().speak()
+        print("Meow!")
+
+dog = Dog("Fido")
+cat = Cat("Whiskers")
+
+#print(dog.name) # Output: Fido
+#print(cat.name) # Output: Whiskers
+
+dog.speak() # Output: Some animal sound\nWoof!
+print() # blank line
+cat.speak() # Output: Some animal sound\nMeow!
